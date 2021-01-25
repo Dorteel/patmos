2adfa5fc42db71005df672ac8ae5cc1a330903ee
Rahul <rravic19@student.aau.dk>
2021-01-25 17:17:52 +0100

    UPDATE: motor test

--- c/apps/de10-nano/test/motor_test.c
+++ c/apps/de10-nano/test/motor_test.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
   for (int i = 0; i < 2; ++i)
   {
 
+    printf("motor 1 spinning\n");
     actuator_write(m1, 1100);                                                 //give motors 1000us pulse.
     actuator_write(m2, 1000);
     actuator_write(m3, 1000);
@@ -24,6 +25,7 @@ int main(int argc, char **argv)
 
     millis(2000);
 
+    printf("motor 2 spinning\n");
     actuator_write(m1, 1000);                                                 //give motors 1000us pulse.
     actuator_write(m2, 1100);
     actuator_write(m3, 1000);
@@ -31,6 +33,7 @@ int main(int argc, char **argv)
 
     millis(2000);
 
+    printf("motor 3 spinning\n");
     actuator_write(m1, 1000);                                                 //give motors 1000us pulse.
     actuator_write(m2, 1000);
     actuator_write(m3, 1100);
@@ -38,6 +41,7 @@ int main(int argc, char **argv)
 
     millis(2000);
 
+    printf("motor 4 spinning\n");
     actuator_write(m1, 1000);                                                 //give motors 1000us pulse.
     actuator_write(m2, 1000);
     actuator_write(m3, 1000);
@@ -45,6 +49,7 @@ int main(int argc, char **argv)
 
     millis(2000);
 
+    printf("all motors spinning\n");
     actuator_write(m1, 1100);                                                 //give motors 1000us pulse.
     actuator_write(m2, 1100);
     actuator_write(m3, 1100);
@@ -53,6 +58,11 @@ int main(int argc, char **argv)
     millis(2000);                                //Set the timer for the next loop.
   
   }
+    printf("motors stop\n");
+    actuator_write(m1, 1000);                                                  //give motors 1000us pulse.
+    actuator_write(m2, 1000);
+    actuator_write(m3, 1000);
+    actuator_write(m4, 1000);
 
 
   return 0;
